c71b80aca7d1ae775d3ee0b653e2abf828ba6c96
Leonardo Barbiero <leonardo.barbiero@gmail.com>
2014-04-24 20:54:37 +0200

    Initial Commit

---
+++ 20140424Barbiero3097/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  20140424Barbiero3097
+//
+//  Created by Leonardo Barbiero on 24/04/14.
+//  Copyright (c) 2014 Leonardo Barbiero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
